edd0f2ec89049edfa08c9f85ff4ab0afb1787130
Caleb Thompson <calebjthompson1230@gmail.com>
2021-11-18 11:37:51 -0600

    Initial Commit

---
+++ leonids-toys.js
@@ -0,0 +1,48 @@
+const toys = [
+    {
+        id: 1,
+        name: "Fallout 4 Pipboy",
+        price: 140,
+        instock: true,
+        weight: 10
+    },
+    {
+        id: 2,
+        name: "Fluffy",
+        price: 10,
+        instock: true,
+        weight: 1.75
+    },
+    {
+        id: 3,
+        name: "Wii",
+        price: 200,
+        instock: false,
+        weight: 15
+    }
+    ]
+
+const rubberDuck = {
+    id: 4,
+    name: "Jeff",
+    price: 5,
+    instock: true,
+    weight: 0.25
+};
+const actionFigure = {
+    id: 5,
+    name: "John Cena",
+    price: 19.99,
+    instock: true,
+    weight: 2
+};
+
+
+//const toyToFind = 2
+
+//for (const toy of toys) {
+//    if (toy.id === toyToFind) {
+//        toy.price = toy.price * 1.05
+//        console.log(`The ${toy.name} costs ${toy.price} dollars.`)
+//    }
+//};
